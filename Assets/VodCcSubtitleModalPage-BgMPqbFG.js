b15478206e9cec3a2abf6c53be347dd95b56b2c3
Kate <grip230@KATE-MacBook-Pro.local>
2026-04-09 21:34:25 +0900

    공통화: 페이지 구조 통일 (ContentHeader 분리 및 디자인 변경, ContentLayout/ContentTitle 신규, AppHeader/AppSidebar 리네이밍, ARCHITECTURE.md 신규)

--- Assets/VodCcSubtitleModalPage-BgMPqbFG.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-DuFlYUda.js";import"./ChangelogModal-CJGmI8aq.js";import"./ContentHeader-DMtBGfH6.js";import"./Toggle-B5uXp8jT.js";import"./ScenarioPanel-93PBcJ5f.js";import"./Badge-Dhke4q5-.js";import"./FormRow-CBquF8So.js";import t from"./VodCcAdminVodContents-DOpUQV8k.js";var n=e();function r(){return(0,n.jsx)(t,{initialModal:!0})}export{r as default};
